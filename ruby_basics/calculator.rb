44f69e33996f369660394d3aa58c980d3e0c34f3
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-01-25 19:13:48 +0200

    more basic ruby

---
+++ ruby_basics/calculator.rb
@@ -0,0 +1,14 @@
+
+puts "Enter a number: "
+num1 =gets.chomp()
+puts "Enter another number: "
+num2 =gets.chomp()
+
+# this will provide a concatenating
+puts num1 + num2
+
+# this will provide a decimal output
+puts (num1.to_f + num2.to_f)
+
+# this will provide a number output
+puts (num1.to_f + num2.to_f)
---
+++ ruby_basics/exercisess.rb
@@ -0,0 +1,4 @@
+ninja_name = "Sasuke"
+ninja_waepon_choice = "The Blade of Tsurugi"
+
+puts ("There was a famous ninja called " + ninja_name)
